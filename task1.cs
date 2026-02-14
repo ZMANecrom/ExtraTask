1b546a6ed70c496fa8bc3952f3fce989db55f066
Zelinskyi Maksym <zelinskyi.ma@gmail.com>
2026-02-14 11:04:11 +0200

    Зробив збір тексту

--- task1.cs
+++ task1.cs
@@ -4,6 +4,13 @@ class task1
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Введіть текст (порожній рядок для завершення): ");
+        string inputText = "";
+        string line;
+        while (!string.IsNullOrEmpty(line = Console.ReadLine()))
+        {
+            inputText += line + " ";
+        }
+        Console.Write(inputText);
     }
 }
